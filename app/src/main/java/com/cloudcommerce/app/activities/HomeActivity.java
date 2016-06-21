fe1291f17afb75fd34aec390c3b7da19bb1011b1
BhagyaLatha <bhagyalatha.m@imaginnovate.com>
2016-06-21 14:10:26 +0530

    Added Home fragment and its reference HomeActivity

--- app/src/main/java/com/cloudcommerce/app/activities/HomeActivity.java
+++ app/src/main/java/com/cloudcommerce/app/activities/HomeActivity.java
@@ -4,11 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cloudcommerce.app.R;
+import com.cloudcommerce.app.datamodels.CloudCommerceSessionData;
+import com.cloudcommerce.app.fragments.HomeFragment;
 
 public class HomeActivity extends BaseNavigationActivity {
+    HomeFragment homeFragment;
+    private String menuTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        menuTitle = getResources().getString(R.string.menu_services);
+        mTitle.setText(menuTitle);
+        CloudCommerceSessionData.getSessionDataInstance().setSelectedMenuTitle(menuTitle);
+        //load home fragment
+        homeFragment = HomeFragment.newInstance();
+        loadFragment(homeFragment, R.id.container_body, "Home");
     }
 }
---
+++ app/src/main/java/com/cloudcommerce/app/fragments/HomeFragment.java
@@ -0,0 +1,58 @@
+package com.cloudcommerce.app.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cloudcommerce.app.R;
+
+public class HomeFragment extends BaseFragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View servicesView = inflater.inflate(R.layout.fragment_home, container, false);
+        initializeControls(servicesView);
+        setDataToControls();
+        return servicesView;
+    }
+
+    private void initializeControls(View servicesView) {
+
+    }
+
+    private void setDataToControls() {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
