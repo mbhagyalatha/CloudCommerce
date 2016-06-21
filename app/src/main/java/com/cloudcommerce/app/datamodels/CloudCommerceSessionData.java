6dd4fc567c8b370cc9ee1157a5a42b35450e4ad1
BhagyaLatha <bhagyalatha.m@imaginnovate.com>
2016-06-21 14:09:23 +0530

    To add application class in manifest file

--- app/src/main/java/com/cloudcommerce/app/datamodels/CloudCommerceSessionData.java
+++ app/src/main/java/com/cloudcommerce/app/datamodels/CloudCommerceSessionData.java
@@ -2,6 +2,7 @@ package com.cloudcommerce.app.datamodels;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.cloudcommerce.app.CloudCommerceApplication;
 import com.google.gson.Gson;
@@ -12,7 +13,7 @@ import com.google.gson.GsonBuilder;
  */
 public class CloudCommerceSessionData {
     private static CloudCommerceSessionData sessionData;
-    public static String MURPHY_APP = "MurphyApp";
+    public static String CLOUD_COMMERCE_APP = "CloudCommerceApp";
     public static String SHPREF_KEY_USER_OBJECT = "SHPREF_KEY_USER_OBJECT";
     public static String SHPREF_KEY_IS_FIRST_LAUNCH = "SHPREF_KEY_IS_FIRST_LAUNCH";
     public static boolean isFirstlaunch;
@@ -26,30 +27,31 @@ public class CloudCommerceSessionData {
         return sessionData;
     }
 
-    public void clearData(){
-        CloudCommerceApplication.getAppContext().getSharedPreferences(MURPHY_APP, Context.MODE_PRIVATE).edit().clear().commit();
+    public void clearData() {
+        CloudCommerceApplication.getAppContext().getSharedPreferences(CLOUD_COMMERCE_APP, Context.MODE_PRIVATE).edit().clear().commit();
     }
 
     public static boolean isFirstlaunch() {
-        boolean isFirstlaunch = CloudCommerceApplication.getAppContext().getSharedPreferences(MURPHY_APP, Context.MODE_PRIVATE).getBoolean(SHPREF_KEY_IS_FIRST_LAUNCH, false);
+        boolean isFirstlaunch = CloudCommerceApplication.getAppContext().getSharedPreferences(CLOUD_COMMERCE_APP, Context.MODE_PRIVATE).getBoolean(SHPREF_KEY_IS_FIRST_LAUNCH, false);
         return isFirstlaunch;
     }
 
     public static void setIsFirstlaunch(boolean isFirstlaunch) {
         CloudCommerceSessionData.isFirstlaunch = isFirstlaunch;
-        SharedPreferences.Editor prefsEditor = CloudCommerceApplication.getAppContext().getSharedPreferences(MURPHY_APP, Context.MODE_PRIVATE).edit();
+        SharedPreferences.Editor prefsEditor = CloudCommerceApplication.getAppContext().getSharedPreferences(CLOUD_COMMERCE_APP, Context.MODE_PRIVATE).edit();
         prefsEditor.putBoolean(SHPREF_KEY_IS_FIRST_LAUNCH, isFirstlaunch);
         prefsEditor.commit();
     }
 
     public static String getSelectedMenuTitle() {
-        String selectedTitle = CloudCommerceApplication.getAppContext().getSharedPreferences(MURPHY_APP, Context.MODE_PRIVATE).getString(SHPREF_KEY_SELECTED_MENU_TITLE, null);
+        String selectedTitle = CloudCommerceApplication.getAppContext().getSharedPreferences(CLOUD_COMMERCE_APP, Context.MODE_PRIVATE).getString(SHPREF_KEY_SELECTED_MENU_TITLE, null);
         return selectedTitle;
     }
 
     public static void setSelectedMenuTitle(String selectedMenuTitle) {
         CloudCommerceSessionData.selectedMenuTitle = selectedMenuTitle;
-        SharedPreferences.Editor prefsEditor = CloudCommerceApplication.getAppContext().getSharedPreferences(MURPHY_APP, Context.MODE_PRIVATE).edit();
+        Log.d("Session data", " context " + CloudCommerceApplication.getAppContext());
+        SharedPreferences.Editor prefsEditor = CloudCommerceApplication.getAppContext().getSharedPreferences(CLOUD_COMMERCE_APP, Context.MODE_PRIVATE).edit();
         prefsEditor.putString(SHPREF_KEY_SELECTED_MENU_TITLE, selectedMenuTitle);
         prefsEditor.commit();
     }
